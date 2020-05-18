ad6fe3b88da99dbfb614405119b3c4db1b240691
Travis Byrman <travis-byrman@live.nmit.ac.nz>
2020-05-19 11:11:38 +1200

    Adding images

---
+++ recursive.js
@@ -0,0 +1,17 @@
+function countDown(number) {
+	for (let i = number; i > 0; i--) {
+		console.log(i);
+	}	
+}
+countDown(10)
+
+/*
+
+create function countDown using the parameter variable of (number) {
+	for (if number is greater than 0 then do the following and -1 from number) {
+		then print the current value
+	}	
+}
+parameter number for function countDown(is 10)
+
+*/
